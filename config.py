872e7220c7c3f527534d9f8b7527ed3a278574f7
Egorka-drug <sashamorozov907@gmail.com>
2026-08-18 13:28:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 TOKEN = os.getenv('TOKEN')
 AI_TOKEN = os.getenv('AI_TOKEN')
 OCR_API_KEY = os.getenv('OCR_API_KEY')
-ADMIN_ID = 11111111
+ADMIN_ID = 6947365047
 gpt_code = os.getenv('GPT_KEY')
 
 
